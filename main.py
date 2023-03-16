36473abd67b2c9a4f7eca3f5fd2f87adf99dbf3a
Andy Bulka <abulka@gmail.com>
2023-03-16 15:17:24 +1100

    cleanup

--- main.py
+++ main.py
@@ -2,26 +2,7 @@ import sys
 import wx
 import subprocess
 import wx.lib.newevent
-# from wx.lib.pubsub import pub # deprecated
 from pubsub import pub  # pip install pypubsub
-
-# ------------ create a listener ------------------
-
-def listener1(arg1, arg2=None):
-    print('Function listener1 received:')
-    print('  arg1 =', arg1)
-    print('  arg2 =', arg2)
-
-# ------------ register listener ------------------
-
-pub.subscribe(listener1, 'rootTopic')
-
-# ---------------- send a message ------------------
-
-print('Publish something via pubsub')
-anObj = dict(a=456, b='abc')
-pub.sendMessage('rootTopic', arg1=123, arg2=anObj)
-
 
 class Commit:
     def __init__(self, sha, date, author, comment):
@@ -31,19 +12,6 @@ class Commit:
         self.comment = comment
 
 current_commit = 'HEAD'
-
-class FileSelectedEvent(wx.PyCommandEvent):
-    def __init__(self, evtType, id, filecontents):
-        wx.PyCommandEvent.__init__(self, evtType, id)
-        self.filecontents = filecontents
-
-EVT_FILE_SELECTED = wx.NewEventType()
-EVT_FILE_SELECTED_CHANGED = wx.PyEventBinder(EVT_FILE_SELECTED, 1)
-
-# RefreshPlantUmlEvent, EVT_REFRESH_PLANTUML_EVENT = wx.lib.newevent.NewEvent()
-
-# SomeNewEvent, EVT_SOME_NEW_EVENT = wx.lib.newevent.NewEvent()
-# SomeNewCommandEvent, EVT_SOME_NEW_COMMAND_EVENT = wx.lib.newevent.NewCommandEvent()
 
 def get_files_in_repo(commit):
     command = ['git', 'ls-tree', '-r', '--name-only', commit]
@@ -138,11 +106,6 @@ class FileTreePanel(wx.Panel):
         # Bind the tree control to the EVT_TREE_SEL_CHANGED event
         self.tree.Bind(wx.EVT_TREE_SEL_CHANGED, self.on_tree_sel_changed)
        
-        # Bind the custom event to the EVT_FILE_SELECTED_CHANGED event
-
-        # self.Bind(EVT_REFRESH_PLANTUML_EVENT, self.refresh_plantuml_view)
-        # self.Bind(EVT_SOME_NEW_EVENT, self.handler)
-
         # Fetch the file tree for the current commit
         files = get_files_in_repo(current_commit)
 
@@ -184,7 +147,6 @@ class FileTreePanel(wx.Panel):
             path = self.tree.GetItemText(item)
             parent = self.tree.GetItemParent(item)
             while parent:
-                print(self.tree.GetItemText(parent))
                 path = self.tree.GetItemText(parent) + '/' + path
                 parent = self.tree.GetItemParent(parent)
 
@@ -199,46 +161,6 @@ class FileTreePanel(wx.Panel):
         contents = self.get_file_contents(current_commit, path)
         # print(contents)
         pub.sendMessage('EVT_FILE_SELECTED_CHANGED', contents=contents)
-        
-        # anObj = dict(a=456, b='abc')
-        # pub.sendMessage('rootTopic', arg1=123, arg2=anObj)
-
-
-        # evt = FileSelectedEvent(EVT_FILE_SELECTED, self.GetId(), contents)
-        # wx.PostEvent(self, evt)
-
-        # # Display the contents in the file contents panel
-        # # Send a custom event to the parent window with the selected file path
-        # evt = FileSelectedEvent(EVT_FILE_SELECTED, self.GetId(), contents)
-        # print('post event', evt)
-        # target = self.GetParent()
-        # print('target', target)
-        # target_parent = target.GetParent()
-        # print('target_parent', target_parent)
-        # target_parent_parent = target_parent.GetParent()
-        # print('target_parent_parent', target_parent_parent)
-        # # ensure target_parent_parent is type wx.Frame
-        # if isinstance(target_parent_parent, wx.Frame):
-        #     print('is wx.Frame, OK')
-        # else:
-        #     print('not wx.Frame, ERROR')
-
-        # # wx.PostEvent(target_parent_parent, evt)
-        # wx.PostEvent(self, evt)
-        # wx.PostEvent(target_parent_parent, RefreshPlantUmlEvent())
-        # # Create the event
-        # evt = SomeNewEvent(attr1="hello", attr2=654)
-        # # Post the event
-        # wx.PostEvent(target_parent_parent, evt)
-        # wx.PostEvent(self, evt)
-
-        # handler = self.GetParent().GetEventHandler()
-        # # if wx.EvtHandler.GetEventHandler().HasEventHandlerFor(EVT_FILE_SELECTED):
-        # if target_parent_parent.GetEventHandler().HasEventHandlerFor(EVT_FILE_SELECTED):
-        # # if target_parent_parent.HasEventHandlers(EVT_FILE_SELECTED):
-        #     print("Handler is registered")
-        # else:
-        #     print("Handler is not registered")
 
     def get_file_contents(self, commit, file_path):
         # get the git command to get the contents of the file at the given commit
@@ -246,28 +168,11 @@ class FileTreePanel(wx.Panel):
         # run the command and return the output
         return subprocess.check_output(command).decode()
 
-    def on_file_selected(self, event):
-        print('on_file_selected: got event', event)
-        file_contents = event.filecontents
-
-        # Display the contents of the selected file in the FileContentsPanel
-        # (assuming the parent window has a reference to the FileContentsPanel)
-        print('self.GetParent().file_contents_panel', self.GetParent().file_contents_panel)
-        self.GetParent().file_contents_panel.display_file_contents(file_contents)
-
-    # def refresh_plantuml_view(self, event):
-    #     print('refresh_plantuml_view: got event', event)
-
-    # def handler(self, evt):
-    #     # Given the above constructed event, the following is true
-    #     print(evt.attr1 == "hello")
-    #     print(evt.attr2 == 654)
 
 class FileContentsPanel(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent, style=wx.SIMPLE_BORDER)
 
-        self.Bind(EVT_FILE_SELECTED_CHANGED, self.on_file_selected)
         pub.subscribe(self.on_file_selected, 'EVT_FILE_SELECTED_CHANGED')
 
         # Set the background color to red
@@ -283,7 +188,6 @@ class FileContentsPanel(wx.Panel):
         self.Layout()
 
     def on_file_selected(self, contents):
-        print('on_file_selected: got event')
         file_contents = contents
 
         # Set the file text display without changing the scroll value
---
+++ research/pubsub/example.py
@@ -0,0 +1,20 @@
+import wx.lib.newevent
+# from wx.lib.pubsub import pub # deprecated
+from pubsub import pub  # pip install pypubsub
+
+# ------------ create a listener ------------------
+
+def listener1(arg1, arg2=None):
+    print('Function listener1 received:')
+    print('  arg1 =', arg1)
+    print('  arg2 =', arg2)
+
+# ------------ register listener ------------------
+
+pub.subscribe(listener1, 'rootTopic')
+
+# ---------------- send a message ------------------
+
+print('Publish something via pubsub')
+anObj = dict(a=456, b='abc')
+pub.sendMessage('rootTopic', arg1=123, arg2=anObj)

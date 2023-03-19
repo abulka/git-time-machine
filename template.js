5683af00f689716e2e7642216e892e355bbefe91
Andy Bulka <abulka@gmail.com>
2023-03-19 12:43:43 +1100

    sending scroll pos to python ok

--- template.js
+++ template.js
@@ -1,2 +1,13 @@
 window.wx_msg.postMessage('This is a message body');
 window.wx_msg.postMessage('This is a message body again'); 
+
+function sendScrollPos() {
+    var scrollPos = window.scrollY;
+    window.wx_msg.postMessage(`we are scrolling at ${scrollPos}`)
+}
+
+window.addEventListener('scroll', sendScrollPos);
+
+function scrollToPos(scrollPos) {
+    window.scrollTo(0, scrollPos);
+}

7c4a86b7bf9b129b013d979fe7d2b96da3778418
Andy Bulka <abulka@gmail.com>
2023-03-24 18:34:06 +1100

    reported bug re treectrl

--- research/treeview/simple-treeview.py
+++ research/treeview/simple-treeview.py
@@ -7,6 +7,17 @@ class MyFrame(wx.Frame):
         # Create the tree control
         self.tree = wx.TreeCtrl(self, style=wx.TR_DEFAULT_STYLE)
 
+        """
+        WXPYTHON BUG REPORT:
+        https://github.com/wxWidgets/Phoenix/issues/2366 
+        wx.TreeCtrl has a bug where the colour of the selected item when the control
+        loses focus is white text on a light grey background. This is a problem for visibility.
+        changing the background or foreground colours of the control does not help.
+        """
+        # self.tree.SetBackgroundColour(wx.LIGHT_GREY)
+        # self.tree.SetBackgroundColour(wx.BLUE)
+        # self.tree.SetForegroundColour(wx.BLACK)
+
         # Add some items to the tree
         root = self.tree.AddRoot("Root")
         item1 = self.tree.AppendItem(root, "Item 1")

86cbf109b1c7ae1d7fe6a21f3b67140cfb51ced4
Andy Bulka <abulka@gmail.com>
2023-03-24 22:35:19 -0400

    snap compiles ok but util module missing

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'git-tm=main:run',
+            'gitm=main:run',
         ],
     },    
 )

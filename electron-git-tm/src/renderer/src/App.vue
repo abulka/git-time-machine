4b13b866a33bbdf1d9a90643b8833d74ae957f18
Andy Bulka <abulka@gmail.com>
2023-04-18 17:47:43 +1000

    moved tailwind template into a component

--- electron-git-tm/src/renderer/src/App.vue
+++ electron-git-tm/src/renderer/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import Versions from './components/Versions.vue'
 import AndyQDialog from './components/AndyQDialog.vue'
+import AndyTailwind from './components/AndyTailwind.vue'
 </script>
 
 <template>
@@ -12,26 +13,7 @@ import AndyQDialog from './components/AndyQDialog.vue'
     <template #actions> extra stuff </template>
   </AndyQDialog>
 
-  <p class="bg-green text-red">HI THERE TAILWIND</p>
-  <div class="w-20">This element has a width of 20px.</div>
-  <div class="border-4 border-red-500">This element has a thick red border.</div>
-  <div class="text-xl">This text is larger.</div>
-  <div class="text-center">This text is centered horizontally.</div>
-  <div class="max-w-screen w-full text-xs pt-5">
-    This element is full width on small screens, and is constrained to the screen width on larger
-    screens. Hippocampes les sans je ce porteurs en de, les courants brides sillage des des qu'on
-    des dispersant marées. D'or éclairs ma inouies quand leurs aux et clapotements lactescent,
-    lorsqu'a hiver tout houle oiseau je. Restais vu repeché verte femme. Sans qu'une la je une
-    maritimes l'eau énormes glauques. Des morves peaux ayant les couleurs en, braises ô blonds les
-    ces aux. Moi d'oiseaux le pôles les au pour, brumes porteurs qu'aux des lors, du brumes fileur
-    plus pôles arbres victimes au des, par zones les avaient frissons fientes des délirants. m'ont
-    éternels de fond.
-  </div>
-
-  <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-    <p class="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
-    <p class="text-gray-500 text-lg">Vue and Tailwind CSS in action</p>
-  </div>
+  <AndyTailwind />
 
   <svg class="hero-logo" viewBox="0 0 900 300">
     <use xlink:href="./assets/icons.svg#electron" />

f7e12f12e0310a48e925c1b028ca8e71b4463ae9
Andy Bulka <abulka@gmail.com>
2023-04-19 10:38:04 +1000

    got icons appearing

--- electron-git-tm/src/renderer/src/components/VuetifyList.vue
+++ electron-git-tm/src/renderer/src/components/VuetifyList.vue
@@ -2,7 +2,30 @@
   <v-app>
     <v-main>
       <v-text-field v-model="msg" />
-      <v-text-field v-model="msg" />
+
+      <v-icon>mdi-clock</v-icon>
+      <v-icon icon="fa:fas fa-plus" />
+      <v-icon icon="fa:fas fa-hashtag" />
+      <v-icon icon="fa:fas fa-eye" />
+      <v-icon icon="mdi:mdi-minus" />
+      <v-icon icon="fa:fas fa-house" />
+      <v-icon icon="fa:fas fa-minus" />
+      <v-icon icon="mdi-vuetify"></v-icon>
+      <v-icon icon="mdi-account"></v-icon>
+      <v-icon icon="mdi-home-clock-outline"></v-icon>
+      <v-icon icon="mdi-ab-testing"></v-icon>
+      <v-icon size="large" color="purple-darken-2" icon="mdi-dialpad"></v-icon>
+      <div class="d-flex justify-space-around">
+        <v-icon icon="fa:fas fa-lock"></v-icon>
+        <v-icon icon="fa:fas fa-search"></v-icon>
+        <v-icon icon="fa:fas fa-list"></v-icon>
+        <v-icon icon="fa:fas fa-edit"></v-icon>
+        <v-icon icon="fa:fas fa-tachometer-alt"></v-icon>
+        <v-icon icon="fa:fas fa-circle-notch fa-spin"></v-icon>
+      </div>
+
+      <!-- add a spacer -->
+      <div class="pb-5"></div>
 
       <v-card class="mx-auto" max-width="300">
         <p>hi there this is a test of vuetify v-list</p>
@@ -19,6 +42,20 @@
           </v-list-item>
         </v-list>
       </v-card>
+
+      <div class="text-center">
+        <v-btn prepend-icon="mdi-check-circle" append-icon="mdi-account-circle">
+          <template v-slot:prepend>
+            <v-icon color="success"></v-icon>
+          </template>
+
+          Button
+
+          <template v-slot:append>
+            <v-icon color="warning"></v-icon>
+          </template>
+        </v-btn>
+      </div>
     </v-main>
   </v-app>
 </template>
@@ -32,5 +69,6 @@ const items = ref([
   { text: 'Real-Time', icon: 'mdi-clock' },
   { text: 'Audience', icon: 'mdi-account' },
   { text: 'Conversions', icon: 'mdi-flag' }
+  // { text: 'Andy', icon: 'flag' }
 ])
 </script>
--- electron-git-tm/src/renderer/src/main.ts
+++ electron-git-tm/src/renderer/src/main.ts
@@ -11,11 +11,24 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import { fa } from 'vuetify/iconsets/fa'
+import '@mdi/font/css/materialdesignicons.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
 const vuetify = createVuetify({
   components,
   directives,
   theme: {
     defaultTheme: 'dark'
+  },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+      fa
+    }
   }
 })
 

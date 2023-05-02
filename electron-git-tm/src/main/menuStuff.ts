d6c6f0d2e2df804e364affbff442e3db2d0f34ae
Andy Bulka <abulka@gmail.com>
2023-05-02 16:00:57 +1000

    save preferences OK

--- electron-git-tm/src/main/menuStuff.ts
+++ electron-git-tm/src/main/menuStuff.ts
@@ -3,25 +3,39 @@ import { changeCwd } from './changeDir'
 import { repoDir, preferences, setPreferences } from './globalsMain'
 import fs from 'fs'
 import path from 'path'
+import { app } from 'electron'
 
 export function startupBusinessLogic(mainWindow): void {
   const { webContents } = mainWindow
 
   // read preferences from file
-  const prefsPath = path.join(__dirname, 'preferences.json');
+  const preferencesPath = getPrefsPath()
+  const prefsPath = path.join(preferencesPath, 'preferences.json')
   let _preferences = {}
   try {
     _preferences = JSON.parse(fs.readFileSync(prefsPath, 'utf8'));
     console.log('preferences', _preferences)
     setPreferences(_preferences)
   } catch (err) {
-    console.error('Error parsing preferences.json', err)
+    console.error('Error parsing preferences.json', prefsPath)
   }
 
   if (repoDir === '') {
     console.log(`${repoDir} is empty, not bothering to get branches`)
   }
   webContents.send('shouldGetBranches', repoDir) // invoke-async-function - or not async?
+}
+
+function getPrefsPath(): string {
+  const preferencesPath = path.join(app.getPath('userData'), 'prefs')
+  console.log(`preferencesPath is ${preferencesPath}`)
+
+  // Create the preferences directory if it doesn't exist
+  if (!fs.existsSync(preferencesPath)) {
+    fs.mkdirSync(preferencesPath)
+    console.log(`${preferencesPath} created`)
+  }
+  return preferencesPath
 }
 
 export function setupMenu(mainWindow): void {
@@ -40,8 +54,10 @@ export function setupMenu(mainWindow): void {
               webContents.send('shouldGetBranches', repoDir) // invoke-async-function - or not async?
               // save preferences
               preferences.repoDir = repoDir
-              const prefsPath = path.join(__dirname, 'preferences.json')
+              const preferencesPath = getPrefsPath()
+              const prefsPath = path.join(preferencesPath, 'preferences.json')
               fs.writeFileSync(prefsPath, JSON.stringify(preferences))
+              console.log(`${prefsPath} written`)
             } else {
               console.error('Cwd change failed')
             }

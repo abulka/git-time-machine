188b2002e68c4297ab3e011533224d0fde8caa28
Andy Bulka <abulka@gmail.com>
2023-05-02 15:51:28 +1000

    beginning of preferences

--- electron-git-tm/src/main/globalsMain.ts
+++ electron-git-tm/src/main/globalsMain.ts
@@ -3,3 +3,14 @@ export let repoDir = '/Users/andy/Devel/pynsource/'
 export function setRepoDir(dir): void {
   repoDir = dir
 }
+
+export let preferences = {
+  theme: 'dark',
+  fontSize: 14,
+  repoDir: repoDir,
+}
+
+export function setPreferences(prefs): void {
+  preferences = prefs
+  if (preferences.repoDir != '') setRepoDir(preferences.repoDir)
+}
--- electron-git-tm/src/main/menuStuff.ts
+++ electron-git-tm/src/main/menuStuff.ts
@@ -1,9 +1,23 @@
 import { Menu, MenuItemConstructorOptions } from 'electron'
 import { changeCwd } from './changeDir'
-import { repoDir } from './globalsMain'
+import { repoDir, preferences, setPreferences } from './globalsMain'
+import fs from 'fs'
+import path from 'path'
 
 export function startupBusinessLogic(mainWindow): void {
   const { webContents } = mainWindow
+
+  // read preferences from file
+  const prefsPath = path.join(__dirname, 'preferences.json');
+  let _preferences = {}
+  try {
+    _preferences = JSON.parse(fs.readFileSync(prefsPath, 'utf8'));
+    console.log('preferences', _preferences)
+    setPreferences(_preferences)
+  } catch (err) {
+    console.error('Error parsing preferences.json', err)
+  }
+
   if (repoDir === '') {
     console.log(`${repoDir} is empty, not bothering to get branches`)
   }
@@ -24,6 +38,10 @@ export function setupMenu(mainWindow): void {
             if (result.success) {
               const { webContents } = mainWindow
               webContents.send('shouldGetBranches', repoDir) // invoke-async-function - or not async?
+              // save preferences
+              preferences.repoDir = repoDir
+              const prefsPath = path.join(__dirname, 'preferences.json')
+              fs.writeFileSync(prefsPath, JSON.stringify(preferences))
             } else {
               console.error('Cwd change failed')
             }

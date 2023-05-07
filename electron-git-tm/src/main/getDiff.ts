9327574f174c1b37f43d424a0c2586522af55d80
Andy Bulka <abulka@gmail.com>
2023-05-07 21:53:12 +1000

    clean out diff logic

--- electron-git-tm/src/main/getDiff.ts
+++ electron-git-tm/src/main/getDiff.ts
@@ -3,12 +3,18 @@ import { exec } from 'child_process'
 import util from 'util'
 import { commits } from './getCommits' // TODO: import from globalsMain
 import Handlebars from 'handlebars'
-import fs from 'fs';
+import fs from 'fs'
 import t3 from '../../resources/templates/template-diff.hbs?asset'
 import t4 from '../../resources/templates/template-diff-js.hbs?asset'
 import { repoDir } from './globalsMain'
 
-function findPreviousCommit(currentCommit: string): string | null {
+const templateSource = fs.readFileSync(t3, 'utf8')
+const template = Handlebars.compile(templateSource) // Compile the template
+
+const templateSourceJs = fs.readFileSync(t4, 'utf8')
+const templateJs = Handlebars.compile(templateSourceJs) // Compile the js template
+
+function _findPreviousCommit(currentCommit: string): string | null {
   for (let i = 0; i < commits.length; i++) {
     if (commits[i].sha.startsWith(currentCommit)) {
     // if (commits[i].sha === currentCommit) { // TODO: use full length sha strings
@@ -22,128 +28,49 @@ function findPreviousCommit(currentCommit: string): string | null {
   return null
 }
 
-// export async generate_html_diff(self):
-
-export async function getDiff(previousCommit: string, currentCommit: string): Promise<string> {
-  const templateSource = fs.readFileSync(t3, 'utf8')
-  const template = Handlebars.compile(templateSource) // Compile the template
-
-  const templateSourceJs = fs.readFileSync(t4, 'utf8')
-  const templateJs = Handlebars.compile(templateSourceJs) // Compile the js template
-
+async function _getDiff(previousCommit: string, currentCommit: string): Promise<string> {
   // call git to get the diff between the two commits
   const execPromisified = util.promisify(exec)
-  const options = { 
+  const options = {
     cwd: repoDir,
     maxBuffer: 10 * 1024 * 1024 // 10MB instead of 200 KB
   }
   const gitCommand = ['git', 'diff', previousCommit, currentCommit]
-  // console.log('gitCommand', gitCommand)
+  let stdout = ''
   try {
-    const { stdout } = await execPromisified(gitCommand.join(' '), options)
-    // console.log(`stdout ${stdout}`)
-
-    // const diffJson = Diff2Html.getJsonFromDiff(stdout)
-    // const diffHtml = Diff2Html.getPrettyHtml(diffJson, { inputFormat: 'json', showFiles: true, matching: 'lines' });
-
-
-    // TODO: IMPLEMENT THIS
-    // hyperlinks = self.extract_hyperlinks(diff_body)
-    // diff_body = self.inject_hyperlinks(diff_body, hyperlinks)
-
-    // toc_template = environment.get_template("links-diff.html")
-    // toc_links = toc_template.render(hyperlinks=hyperlinks, add_filename_to_link=add_filename_to_link)
-
-    // uses Diff2Html https://morioh.com/p/a8623fc10324
-    const jsFileContents = templateJs({ diff_body: stdout })
-
-    const data = {
-      diff_body: stdout,
-      toc_links: '',
-      git_cmd: gitCommand.join(' '),
-      js: jsFileContents
-    }
-
-    if (data.diff_body.match(/[\x00-\x08\x0E-\x1F]/)) { // eslint-disable-line no-control-regex
-      data.diff_body = 'binary characters detected - cannot display diff.'
-    }
-    if (data.diff_body.length > 100000) {
-      data.diff_body = 'diff too long'
-    }
-
-    const renderedTemplate = template(data)
-    return renderedTemplate
-
-    // let gitOutput = stdout
-
-    // // decode the output from bytes to a string
-    // gitOutput = gitOutput.toString()
-
-    // // add span tags for highlighting "-" and "+" lines
-    // const highlightedLines = []
-    // const templateColoured = '<span style="color:{{textColour}}">{{line | escape}}</span>'
-    // const templateUntouched = '{{line | escape}}'
-
-    // gitOutput.split('\n').forEach((line) => {
-    //   if (line.length >= 1 && ['+', '-'].includes(line[0])) {
-    //     if (line.length > 1 && ['+', '-'].includes(line[1])) {
-    //       highlightedLines.push(env.filters.escape(line))
-    //     } else {
-    //       const textColour = line[0] === '+' ? 'lightgreen' : 'red'
-    //       const renderedLine = env.renderString(templateColoured, {
-    //         line,
-    //         textColour
-    //       })
-    //       highlightedLines.push(renderedLine)
-    //     }
-    //   } else {
-    //     highlightedLines.push(env.renderString(templateUntouched, { line }))
-    //   }
-    // })
-
-    // join the lines back into a string with newline separators
-    // gitOutput = highlightedLines.join('\n')
-
-    // // return the diff with highlighted lines
-    // return `${gitCommand.join(' ')}\n\n${gitOutput}`
+    ;({ stdout } = await execPromisified(gitCommand.join(' '), options))
   } catch (e) {
     console.log(`Error getting diff: ${(e as Error).message}`)
-    return ''
   }
+  return stdout
+}
+
+function _renderHtml(stdout, gitCommand): string {
+  // uses Diff2Html https://morioh.com/p/a8623fc10324
+  const jsFileContents = templateJs({ diff_body: stdout })
+
+  const data = {
+    diff_body: stdout,
+    toc_links: '',
+    git_cmd: gitCommand.join(' '),
+    js: jsFileContents
+  }
+
+  if (data.diff_body.match(/[\x00-\x08\x0E-\x1F]/)) { // eslint-disable-line no-control-regex
+    data.diff_body = 'binary characters detected - cannot display diff.'
+  }
+
+  const renderedTemplate = template(data)
+  return renderedTemplate
 }
 
 export async function generate_html_diff(currentCommit: string): Promise<string> {
-  const commit = findPreviousCommit(currentCommit)
+  const commit = _findPreviousCommit(currentCommit)
   if (commit) {
-    const diff = await getDiff(commit, currentCommit)
-    return diff
+    const diff = await _getDiff(commit, currentCommit)
+    const gitCommand = ['git', 'diff', commit, currentCommit]
+    const renderedTemplate = _renderHtml(diff, gitCommand)
+    return renderedTemplate
   }
   return ''
 }
-
-// def generate_html_diff(self):
-//   if event_debug:
-//       print('   commit_changed ->', 'on_show_diff')
-//   # call git to find the sha of the previous commit to current_commit sha
-//   previous_commit = self.get_previous_commit(current_commit)
-
-//   # call git to get the diff between the two commits
-//   diff_body = self.get_diff(previous_commit, current_commit)
-
-//   hyperlinks = self.extract_hyperlinks(diff_body)
-//   diff_body = self.inject_hyperlinks(diff_body, hyperlinks)
-
-//   toc_template = environment.get_template("links-diff.html")
-//   toc_links = toc_template.render(hyperlinks=hyperlinks, add_filename_to_link=add_filename_to_link)
-
-//   js_file_contents = environment.get_template("template-diff.js").render()
-
-//   html_template = environment.get_template("template-diff.html")
-//   html_str = html_template.render(toc_links=toc_links, diff_body=diff_body, js=js_file_contents)
-
-//   if html_debug:
-//       with open('junk-diff.html', 'w') as f:
-//           f.write(html_str)
-
-//   # Set the HTML content
-//   self.html.SetPage(html_str, "")
